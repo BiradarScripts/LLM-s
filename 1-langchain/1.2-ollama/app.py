73c3109e4ae59b27c8236539285c9842c89a995c
Shreyas Biradar <119810773+BiradarScripts@users.noreply.github.com>
2024-09-19 22:07:39 +0000

    update moondream

--- 1-langchain/1.2-ollama/app.py
+++ 1-langchain/1.2-ollama/app.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import load_dotenv
+import requests
+from requests.exceptions import ConnectionError
 
 from langchain_community.llms import Ollama
 import streamlit as st
@@ -9,29 +11,32 @@ from langchain_core.output_parsers import StrOutputParser
 load_dotenv()
 
 ## Langsmith Tracking
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
 
 ## Prompt Template
-prompt=ChatPromptTemplate.from_messages(
+prompt = ChatPromptTemplate.from_messages(
     [
-        ("system","You are a helpful assistant. Please respond to the question asked"),
-        ("user","Question:{question}")
+        ("system", "You are a helpful assistant. Please respond to the question asked"),
+        ("user", "Question:{question}")
     ]
 )
 
 ## streamlit framework
 st.title("Langchain Demo With Gemma Model")
-input_text=st.text_input("What question you have in mind?")
+input_text = st.text_input("What question you have in mind?")
 
-
-## Ollama Llama2 model
-llm=Ollama(model="gemma:2b")
-output_parser=StrOutputParser()
-chain=prompt|llm|output_parser
+# ## Ollama Llama2 model
+llm = Ollama(model="moondream")
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
 
 if input_text:
-    st.write(chain.invoke({"question":input_text}))
-
-
+    try:
+        result = chain.invoke({"question": input_text})
+        st.write(result)
+    except ConnectionError:
+        st.error("Failed to connect to the Ollama service. Please ensure the service is running.")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
